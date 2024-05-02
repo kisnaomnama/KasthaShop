08d99d5136832496209fc65b7d3f06be1059af5b
Krishna <kisnasur@gmail.com>
2024-05-02 13:31:56 -0700

    dist build added assets

--- react-vite/src/components/Product/Product.jsx
+++ react-vite/src/components/Product/Product.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import "./Product.css"
-import { NavLink } from "react-router-dom"
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import "./Product.css"
+// import { NavLink } from "react-router-dom"
 
 
-function Product() {
-    const dispatch = useDispatch();
-    return (
-        <div>
-            This is the Product Page
-        </div>
-    );
-}
+// function Product() {
+//     const dispatch = useDispatch();
+//     return (
+//         <div>
+//             This is the Product Page
+//         </div>
+//     );
+// }
 
-export default Product;
+// export default Product;
